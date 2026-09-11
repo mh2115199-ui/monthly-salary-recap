e12e7e32fbf8802965415a94fedf4f1e3dc908d1
mh2115199-ui <mh2115199@gmail.com>
2026-09-11 18:54:47 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = "monthly-salary-recap-v2";
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache =>
+    cache.addAll(["./", "./index.html", "./manifest.json", "./app-icon-v2.png", "./app-icon-v2-512.png"])
+  ));
+});
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    )).then(() => self.clients.claim())
+  );
+});
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(() => {});
+      return response;
+    }).catch(() => caches.match(event.request))
+  );
+});
